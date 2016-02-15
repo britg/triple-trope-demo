890692e700661a86334baf8436fd7306b9bc5319
britg <brit@britg.com>
2016-02-15 16:06:57 -0600

    Initial commit

---
+++ Assets/Scripts/GridCellHighlight.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using HighlightingSystem;
+
+public class GridCellHighlight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	   var highlighter = GetComponent<Highlighter>();
+       highlighter.ConstantOn(Color.gray, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
