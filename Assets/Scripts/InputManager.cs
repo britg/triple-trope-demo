07f5452f7703c80c57f40dc1696aaeb8defc9f8c
britg <brit@britg.com>
2016-02-16 14:46:11 -0600

    Detect tile clicks

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+    
+    public bool enabled = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   if (enabled) {
+           DetectInput();
+       }
+	}
+    
+    void DetectInput () {
+        if (Input.GetMouseButtonUp(0)) {
+            RaycastHit hit;
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            var layerMask = 1 << 8;
+            if (Physics.Raycast(ray, out hit, 100, layerMask)) {
+                Debug.Log("Hit a tile");
+            }
+            
+        }
+    }
+}
