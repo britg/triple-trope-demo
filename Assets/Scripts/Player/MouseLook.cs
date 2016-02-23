1795645394e7177bb55efbe4c80e6c788920a08d
britg <brit@britg.com>
2016-02-23 13:22:15 -0600

    Reset

---
+++ Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+    
+    int groundLayer = 1 << 8;
+    int dist = 1000;
+    
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+       RaycastHit hit;
+       
+       if (Physics.Raycast(ray, out hit, dist, groundLayer)) {
+           var pos = hit.point;
+           player.transform.LookAt(pos);
+       }
+	}
+}
